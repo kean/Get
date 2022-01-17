dce442074456a68bf1312efd0e021fd3cdd0ae07
simorgh3196 <simorgh3196@gmail.com>
2022-01-17 16:02:43 +0900

    Add tests for APIClientDelegate

--- Tests/GetTests/ClientAuthorizationTests.swift
+++ Tests/GetTests/ClientAuthorizationTests.swift
@@ -18,45 +18,93 @@ final class APIClientAuthorizationTests: XCTestCase {
             $0.sessionConfiguration.protocolClasses = [MockingURLProtocol.self]
         }
     }
-    
-    func testAuthorizationHeaderIsPassed() async throws {
-        // GIVEN
-        let url = URL(string: "https://api.github.com/user")!
-        var mock = Mock(url: url, dataType: .json, statusCode: 401, data: [
-            .get: "Unauthorized".data(using: .utf8)!
-        ])
-        
-        mock.onRequest = { request, arguments in
-            XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer: expired-token")
 
-            self.delegate.token = "valid-token"
-            var mock = Mock.get(url: url, json: "user")
-            mock.onRequest = { request, arguments in
-                XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer: valid-token")
-            }
-            mock.register()
+    func testAuthorizationHeaderWidhValidToken() async throws {
+        // GIVEN
+        delegate.token = Token(value: "valid-token", expiresDate: Date(timeIntervalSinceNow: 1000))
+        let url = URL(string: "https://api.github.com/user")!
+        var mock = Mock.get(url: url, json: "user")
+        mock.onRequest = { request, arguments in
+            XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer valid-token")
+        }
+        mock.register()
+
+        // WHEN
+        try await client.send(.get("/user"))
+    }
+    
+    func testAuthorizationHeaderWithExpiredToken() async throws {
+        // GIVEN
+        delegate.token = Token(value: "expired-token", expiresDate: Date(timeIntervalSinceNow: -1000))
+        let url = URL(string: "https://api.github.com/user")!
+        var mock = Mock.get(url: url, json: "user")
+        mock.onRequest = { request, arguments in
+            XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer valid-token")
         }
         mock.register()
         
         // WHEN
-        let user: User = try await client.send(.get("/user")).value
-                                               
-        // THEN
-        XCTAssertEqual(user.login, "kean")
+        try await client.send(.get("/user"))
+    }
+
+    func testAuthorizationHeaderWithInvalidToken() async throws {
+        // GIVEN
+        delegate.token = Token(value: "invalid-token", expiresDate: Date(timeIntervalSinceNow: 1000))
+        let url = URL(string: "https://api.github.com/user")!
+        var mock = Mock(url: url, dataType: .json, statusCode: 401, data: [
+            .get: "Unauthorized".data(using: .utf8)!
+        ])
+        mock.onRequest = { request, arguments in
+            XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer invalid-token")
+
+            var mock = Mock.get(url: url, json: "user")
+            mock.onRequest = { request, arguments in
+                XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], "Bearer valid-token")
+            }
+            mock.register()
+        }
+        mock.register()
+
+        // WHEN
+        try await client.send(.get("/user"))
     }
 }
 
 private final class MockAuthorizingDelegate: APIClientDelegate {
-    var token = "expired-token"
-    
+    var token: Token!
+    let tokenRefresher = TokenRefresher()
+
     func client(_ client: APIClient, willSendRequest request: inout URLRequest) async throws {
-        request.allHTTPHeaderFields = ["Authorization": "Bearer: \(token)"]
+        let now = Date()
+
+        // Refresh the token if it has expired.
+        if token.expiresDate < now {
+            token = try await tokenRefresher.refreshToken()
+        }
+
+        request.addValue("Bearer \(token.value)", forHTTPHeaderField: "Authorization")
     }
     
-    func shouldClientRetry(_ client: APIClient, withError error: Error) async -> Bool {
+    func shouldClientRetry(_ client: APIClient, withError error: Error) async throws -> Bool {
         if case .unacceptableStatusCode(let statusCode) = (error as? APIError), statusCode == 401 {
+            token = try await tokenRefresher.refreshToken()
             return true
         }
         return false
+    }
+}
+
+private struct Token {
+    /// Value for authorization header.
+    var value: String
+    /// Expiration date of the token. Even within the expiration date,the token may
+    /// have been invalidated in the server.
+    var expiresDate: Date
+}
+
+private struct TokenRefresher {
+    func refreshToken() async throws -> Token {
+        // TODO: Refresh (make sure you only refresh once if multiple requests fail)
+        Token(value: "valid-token", expiresDate: Date(timeIntervalSinceNow: 1000))
     }
 }
