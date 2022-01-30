0392d9a2b08cea575601a7333e366823c0bcf5c3
simorgh3196 <simorgh3196@gmail.com>
2022-01-30 23:39:00 +0900

    Add tests for AuthenticatorInterceptor

--- Tests/GetTests/AuthenticationInterceptorTests.swift
+++ Tests/GetTests/AuthenticationInterceptorTests.swift
@@ -8,26 +8,138 @@ import Mocker
 
 final class AuthenticationInterceptorTests: XCTestCase {
     private var client: APIClient!
-    private var delegate: AuthenticationInterceptor<StubAuthenticator>!
+    private var authenticator =  StubAuthenticator()
 
     override func setUp() {
         super.setUp()
 
-        delegate = AuthenticationInterceptor(authenticator: StubAuthenticator())
-
         client = APIClient(host: "example.com") {
-            $0.delegate = delegate
+            $0.delegate = AuthenticationInterceptor(authenticator: authenticator)
             $0.sessionConfiguration.protocolClasses = [MockingURLProtocol.self]
         }
     }
 
-    func testRefreshTokenOnlyOnceForParallelRequests() async throws {
+    func testAuthorizeRequest() async throws {
         // GIVEN
-        delegate.authenticator.token = .invalidToken
-        Mock.get(url: URL(string: "https://example.com/user?access_token=refreshed-invalid-token")!,
+        Mock.get(url: URL(string: "https://example.com/user?token=refreshed-token")!,
                  statusCode: 200,
                  json: "user").register()
-        Mock.get(url: URL(string: "https://example.com/user?access_token=invalid-token")!,
+
+        // WHEN
+        try await self.client.send(.get("/user"))
+
+        // THEN
+        XCTAssertEqual(authenticator.credential.value, "refreshed-token")
+        XCTAssertEqual(authenticator.refreshCount, 0)
+    }
+
+    func testRefreshAndRetryRequestWhenCredentialIsInvalid() async throws {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        Mock.get(url: URL(string: "https://example.com/user?token=refreshed-token")!,
+                 statusCode: 200,
+                 json: "user").register()
+        Mock.get(url: URL(string: "https://example.com/user?token=token")!,
+                 statusCode: 401,
+                 message: "Unauthorized").register()
+
+        // WHEN
+        try await self.client.send(.get("/user"))
+
+        // THEN
+        XCTAssertEqual(authenticator.credential.value, "refreshed-token")
+        XCTAssertEqual(authenticator.refreshCount, 1)
+    }
+
+    func testThrowErrorWhenCredentialIsInvalid() async {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        Mock.get(url: URL(string: "https://example.com/user?token=refreshed-token")!,
+                 statusCode: 401,
+                 message: "Unauthorized").register()
+        Mock.get(url: URL(string: "https://example.com/user?token=token")!,
+                 statusCode: 401,
+                 message: "Unauthorized").register()
+
+        do {
+            // WHEN
+            try await self.client.send(.get("/user"))
+            XCTFail("Should throw an error")
+        } catch {
+            // THEN
+            XCTAssertEqual(authenticator.credential.value, "refreshed-token")
+            XCTAssertEqual(authenticator.refreshCount, 1)
+            guard case APIError.unacceptableStatusCode(401) = error else {
+                XCTFail("Should receive http status code 401")
+                return
+            }
+        }
+    }
+
+    func testThrowErrorWhenFailToGetCredential() async {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        authenticator.shouldFailToGetCredential = true
+
+        do {
+            // WHEN
+            try await self.client.send(.get("/user"))
+            XCTFail("Should throw an error")
+        } catch {
+            // THEN
+            XCTAssertEqual(authenticator.credential.value, "token")
+            XCTAssertEqual(authenticator.refreshCount, 0)
+            XCTAssertEqual((error as? AuthenticationError)?.reason, .loadingCredentialFailed)
+        }
+    }
+
+    func testThrowErrorWhenFailToApplyCredential() async {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        authenticator.shouldFailToApplyCredential = true
+
+        do {
+            // WHEN
+            try await self.client.send(.get("/user"))
+            XCTFail("Should throw an error")
+        } catch {
+            // THEN
+            XCTAssertEqual(authenticator.credential.value, "token")
+            XCTAssertEqual(authenticator.refreshCount, 0)
+            XCTAssertEqual((error as? AuthenticationError)?.reason, .applyingCredentialFailed)
+        }
+    }
+
+    func testThrowErrorWhenFailToRefreshCredential() async {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        authenticator.shouldFailToRefreshCredential = true
+        Mock.get(url: URL(string: "https://example.com/user?token=refreshed-token")!,
+                 statusCode: 200,
+                 json: "user").register()
+        Mock.get(url: URL(string: "https://example.com/user?token=token")!,
+                 statusCode: 401,
+                 message: "Unauthorized").register()
+
+        do {
+            // WHEN
+            try await self.client.send(.get("/user"))
+            XCTFail("Should throw an error")
+        } catch {
+            // THEN
+            XCTAssertEqual(authenticator.credential.value, "token")
+            XCTAssertEqual(authenticator.refreshCount, 0)
+            XCTAssertEqual((error as? AuthenticationError)?.reason, .refreshingCredentialFailed)
+        }
+    }
+
+    func testRefreshCredentialOnlyOnceForParallelRequests() async throws {
+        // GIVEN
+        authenticator.credential = .invalidCredetial
+        Mock.get(url: URL(string: "https://example.com/user?token=refreshed-token")!,
+                 statusCode: 200,
+                 json: "user").register()
+        Mock.get(url: URL(string: "https://example.com/user?token=token")!,
                  statusCode: 401,
                  message: "Unauthorized").register()
 
@@ -39,28 +151,41 @@ final class AuthenticationInterceptorTests: XCTestCase {
         }
 
         // THEN
-        XCTAssertEqual(delegate.authenticator.token.value, "refreshed-invalid-token")
-        XCTAssertEqual(delegate.authenticator.refreshCount, 1)
+        XCTAssertEqual(authenticator.credential.value, "refreshed-token")
+        XCTAssertEqual(authenticator.refreshCount, 1)
     }
 }
 
 private class StubAuthenticator: Authenticator {
-    typealias Credential = Token
+    typealias Credential = TestCredential
 
-    var token: Token!
-
-    var refreshCount = 0
-
-    func credential() async throws -> Token { token }
-
-    func refresh(credential: Credential, for client: APIClient) async throws -> Credential {
-        token = Token(value: "refreshed-\(credential.value)", expiresDate: Date(timeIntervalSinceNow: 1000))
-        refreshCount += 1
-        return token
+    enum TestError: Error {
+        case error
     }
 
-    func apply(_ credential: Token, to request: inout URLRequest) async throws {
-        request.setQueryItems([.init(name: "access_token", value: credential.value)])
+    var credential: TestCredential = .validCredential
+
+    var shouldFailToGetCredential = false
+    var shouldFailToApplyCredential = false
+    var shouldFailToRefreshCredential = false
+    var refreshCount = 0
+
+    func credential() async throws -> Credential {
+        guard !shouldFailToGetCredential else { throw TestError.error }
+        return credential
+    }
+
+    func apply(_ credential: Credential, to request: inout URLRequest) async throws {
+        guard !shouldFailToApplyCredential else { throw TestError.error }
+        request.setQueryItems([.init(name: "token", value: credential.value)])
+    }
+
+    func refresh(credential: Credential, for client: APIClient) async throws -> Credential {
+        guard !shouldFailToRefreshCredential else { throw TestError.error }
+        self.credential = TestCredential(value: "refreshed-\(credential.value)",
+                                         expiresDate: Date(timeIntervalSinceNow: 1000))
+        refreshCount += 1
+        return self.credential
     }
 
     func didRequest(_: URLRequest, failDueToAuthenticationError error: Error) -> Bool {
@@ -70,26 +195,29 @@ private class StubAuthenticator: Authenticator {
         return false
     }
 
-    func isRequest(_ urlRequest: URLRequest, authenticatedWith credential: Token) -> Bool {
+    func isRequest(_ urlRequest: URLRequest, authenticatedWith credential: Credential) -> Bool {
         guard let url = urlRequest.url,
               let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
               let queryItems = components.queryItems
         else { return false }
 
-        return queryItems.contains(where: { $0.name == "access_token" && $0.value == credential.value })
+        return queryItems.contains(where: { $0.name == "token" && $0.value == credential.value })
     }
 }
 
-private struct Token: Equatable {
+private struct TestCredential: Equatable {
     /// Value for authorization header.
     var value: String
     /// Expiration date of the token. Even within the expiration date,the token may
     /// have been invalidated in the server.
     var expiresDate: Date
 
-    static let validToken = Token(value: "valid-token", expiresDate: Date(timeIntervalSinceNow: 1000))
-    static let invalidToken = Token(value: "invalid-token", expiresDate: Date(timeIntervalSinceNow: 1000))
-    static let expiredToken = Token(value: "valid-token", expiresDate: Date(timeIntervalSinceNow: -1000))
+    static let validCredential = TestCredential(value: "refreshed-token",
+                                                expiresDate: Date(timeIntervalSinceNow: 1000))
+    static let invalidCredetial = TestCredential(value: "token",
+                                                 expiresDate: Date(timeIntervalSinceNow: 1000))
+    static let expiredCredential = TestCredential(value: "refreshed-token",
+                                                  expiresDate: Date(timeIntervalSinceNow: -1000))
 }
 
 private extension URLRequest {
