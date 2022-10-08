1ddf96f67323ad16ce882c1374c766ed05097ced
Oscar De Moya <oscar.demoya@koombea.com>
2022-10-08 15:50:37 -0500

    Updated decode and encode functions to avoid forced downcasting

--- Sources/Get/DataLoader.swift
+++ Sources/Get/DataLoader.swift
@@ -337,30 +337,31 @@ extension Optional: OptionalDecoding {}
 func encode(_ value: Encodable, using encoder: JSONEncoder) async throws -> Data? {
     if let data = value as? Data {
         return data
-    } else if let string = value as? String {
-        return string.data(using: .utf8)
-    } else {
-        return try await Task.detached {
-            try encoder.encode(AnyEncodable(value: value))
-        }.value
     }
+    if let string = value as? String {
+        return string.data(using: .utf8)
+    }
+    return try await Task.detached {
+        try encoder.encode(AnyEncodable(value: value))
+    }.value
 }
 
 func decode<T: Decodable>(_ data: Data, using decoder: JSONDecoder) async throws -> T {
-    if data.isEmpty, T.self is OptionalDecoding.Type {
-        return Optional<Decodable>.none as! T
-    } else if T.self == Data.self {
-        return data as! T
-    } else if T.self == String.self {
-        guard let string = String(data: data, encoding: .utf8) else {
+    if data.isEmpty, T.self is OptionalDecoding.Type, let value = Optional<Decodable>.none as? T {
+        return value
+    }
+    if T.self == Data.self, let value = data as? T {
+        return value
+    }
+    if T.self == String.self {
+        guard let string = String(data: data, encoding: .utf8), let value = string as? T else {
             throw URLError(.badServerResponse)
         }
-        return string as! T
-    } else {
-        return try await Task.detached {
-            try decoder.decode(T.self, from: data)
-        }.value
+        return value
     }
+    return try await Task.detached {
+        try decoder.decode(T.self, from: data)
+    }.value
 }
 
 /// With iOS 16, there is now a delegate method (`didCreateTask`) that gets
